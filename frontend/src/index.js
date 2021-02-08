00026168be846a89458806f3cfadf2cfe772dd01
viktor <radchenkovictor@yahoo.com>
2021-02-08 16:25:54 +0400

    connect custom store with provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./components/app";
 
+import store from "./store";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
