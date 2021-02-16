566e89ad80423474644a5c196398172e763b39a7
viktor <radchenkovictor@yahoo.com>
2021-02-16 17:30:07 +0400

    extended user serializers

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.get_routes, name='routes'),
-    path('products/', views.get_products, name='products'),
-    path('products/<str:pk>/', views.get_product, name='product'),
-]
