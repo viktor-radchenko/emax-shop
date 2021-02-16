4fd326712a8c734d9e440d000876e1d5081cc40a
viktor <radchenkovictor@yahoo.com>
2021-02-16 17:31:04 +0400

    added serializers

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,1 +1,6 @@
 from .product import ProductSerializer  # noqa 401
+from .user import (  # noqa 401
+    UserSerializer,
+    MyTokenObtainPairSerializer,
+    UserSerializerWithToken,
+)
---
+++ api/serializers/user.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from api.models import CustomUser
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token["username"] = user.username
+        token["email"] = user.email
+        return token
+
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    is_admin = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'name', 'is_admin']
+
+    # custom user fields
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+    def get_is_admin(self, obj):
+        return obj.is_staff
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'name', 'is_admin', 'token']
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
