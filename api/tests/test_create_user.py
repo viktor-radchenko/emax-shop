7e5238b08f148a49f163cd134ac79ff37b22372a
viktor <radchenkovictor@yahoo.com>
2021-02-18 00:28:34 +0400

    initial python fixtures

--- api/tests/test_create_user.py
+++ api/tests/test_create_user.py
@@ -1,11 +1,40 @@
+import uuid
+
 import pytest
 
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 
+@pytest.fixture
+def test_password():
+    return 'test-strong-password'
+
+
+@pytest.fixture
+def create_user(db, django_user_model, test_password):
+    def make_user(**kwargs):
+        kwargs['password'] = test_password
+        if 'username' not in kwargs:
+            kwargs['username'] = str(uuid.uuid4())
+        if 'email' not in kwargs:
+            kwargs['email'] = 'test@test.com'
+        if 'first_name' not in kwargs:
+            kwargs['first_name'] = "Sam"
+        if 'is_active' not in kwargs:
+            kwargs['is_active'] = True
+        return django_user_model.objects.create_user(**kwargs)
+    return make_user
+
+
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+    return APIClient()
+
+
 def test_user_routes(client):
-    url = reverse('token_obtain_pair')
+    url = reverse('login')
     response = client.get(url)
     assert response.status_code == 405
 
@@ -38,3 +67,53 @@ def test_create_super_user():
     assert admin_user.is_active
     assert admin_user.is_staff
     assert admin_user.is_superuser
+
+
+@pytest.mark.django_db
+def test_create_user_rest_api(client):
+    url = reverse('register')
+    email = "test@test.com"
+    username = "PytestTest"
+    first_name = "Sam"
+    data = {
+        "email": email,
+        "username": username,
+        "first_name": first_name
+    }
+    response = client.post(url, data)
+    assert response.status_code == 200
+    assert response.data.get("username") == username
+
+
+@pytest.mark.django_db
+def test_fail_create_user(client, create_user):
+    # can't create user with existing credentials
+    user = create_user(username='PytestTest')
+    url = reverse('register')
+    data = {
+        "email": user.email,
+        "username": user.username,
+        "first_name": user.first_name
+    }
+    response = client.post(url, data)
+    assert response.status_code == 400
+    assert response.data.get("detail") == 'User with this email is already registered'
+
+
+@pytest.mark.django_db
+def test_user_login(api_client, create_user, test_password):
+    user = create_user()
+    url = reverse('login')
+    data = {
+        'email': user.email,
+        'password': test_password
+    }
+    response = api_client.post(url, data=data)
+    assert response.status_code == 200
+    url = reverse('profile')
+    headers = {
+        'HTTP_AUTHORIZATION': f'Bearer {response.data.get("access")}'
+    }
+    response = api_client.get(url, **headers)
+    assert response.status_code == 200
+    assert response.data.get("email") == user.email
--- api/urls/auth.py
+++ api/urls/auth.py
@@ -8,7 +8,7 @@ from api import views
 
 urlpatterns = [
     # custom claims for JWT token defined in api.views.user
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', views.MyTokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('register/', views.register_user, name='register'),
