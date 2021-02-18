c0f0bc6ba7c564852599a9fcd977e00c8d483f47
viktor <radchenkovictor@yahoo.com>
2021-02-18 20:14:38 +0400

    user auth stroe

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,27 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import jwt_decode from "jwt-decode";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer, productDetailsReducer, cartReducer } from "./reducers";
+import { productListReducer, productDetailsReducer, cartReducer, userLoginReducer } from "./reducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userInfo: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [];
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? jwt_decode(JSON.parse(localStorage.getItem("userInfo")).access)
+  : null;
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
+  },
+  userInfo: {
+    userInfo: userInfoFromStorage,
   },
 };
 
