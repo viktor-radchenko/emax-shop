f36ed0f0b275e33d38aa65a81ab7b0137a88244a
viktor <radchenkovictor@yahoo.com>
2021-03-01 22:57:46 +0200

    reducer logs

--- frontend/src/reducers/user-reducer.js
+++ frontend/src/reducers/user-reducer.js
@@ -16,6 +16,7 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_REQUEST:
       return { loading: true };
     case USER_LOGIN_SUCCESS:
+      console.log("Applying login success reducer and saving payload to store", action.payload);
       return { loading: false, userInfo: action.payload };
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
